2fc923da5d1c20a8bba1d5e105620f905ce1299c
Kenshin_Bryz <84550697+BryLim-AI@users.noreply.github.com>
2022-12-24 14:45:58 +0800

    Added Initial Components and Views

--- kairui/src/App.vue
+++ kairui/src/App.vue
@@ -1,19 +1,34 @@
 <template>
-  <nav>
+  <div>
+  <nav class="navigation">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
+  <div class="sideNav">
+
+    <a href="" target="_blank">
+      <img class="side-icon" src="@/assets/e.png">
+    </a>
+
+    <a href="" target="_blank">
+      <img class="side-icon" src="@/assets/cal.png">
+    </a>
+
+    <a href="" target="_blank">
+      <img class="side-icon" src="@/assets/map.png">
+    </a>
+
+    <a href="" target="_blank">
+      <img class="side-icon" src="@/assets/tras.png">
+    </a>
+
+  </div>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import "./assets/styles.scss";
 
 nav {
   padding: 30px;
---
+++ kairui/src/views/Academics.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="academics">
+        <h1 class="sec-title">Academic Circles</h1>
+
+        <div class="section-selects">
+
+            <a href="https://bigsky.benilde.edu.ph/d2l/login" target="_blank" class="sec-links">
+                <img class="select-img" src="@/assets/books.png">
+                <p class="sect-sub">BigSky Benilde</p>
+            </a>
+            <a href="https://apps.benilde.edu.ph/sis/" target="_blank" class="sec-links">
+                <img class="select-img" src="@/assets/std.png">
+                <p class="sect-sub">SIS Benilde</p>
+            </a>
+            <a href="https://apps1.benilde.edu.ph/payonline" target="_blank" class="sec-links">
+                <img class="select-img" src="@/assets/pay.png">
+                <p class="sect-sub">Benilde Pay</p>
+            </a>
+            <a href="https://clr-scheds.benilde.edu.ph/" target="_blank" class="sec-links">
+                <img class="select-img" src="@/assets/lib.png">
+                <p class="sect-sub">LaRCy</p>
+            </a>
+
+        </div>
+
+    </div>
+</template>
